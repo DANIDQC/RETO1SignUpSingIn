1b5ba708b470e9ac8e72b8cc016b36e1e370c304
2dam <2dam@HZ375744.TartangaLH.eus>
2024-10-04 12:15:47 +0200

    Commit prueba

---
+++ RetoSignUpSignIn/src/model/User.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *Esta clase contiene información personal de un usuario
+ * @author oscar
+ */
+
+//Clase que encapsula la informacion del usuario
+
+public class User {
+
+    private String usuario;
+    private String contrasena;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+}
--- RetoSignUpSignIn/src/retosignupsignin/RetoSignUpSignIn.java
+++ RetoSignUpSignIn/src/retosignupsignin/RetoSignUpSignIn.java
@@ -15,8 +15,7 @@ public class RetoSignUpSignIn {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //Prueba
+        
     }
     
 }
